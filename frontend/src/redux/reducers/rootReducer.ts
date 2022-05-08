a8c314b399ea50b00a48e07f57db33cc9e771361
Chychkan Tetiana <ChychkanTetiana@Chychkans-MacBook-Pro.local>
2022-05-08 13:07:29 +0300

    added redux

---
+++ frontend/src/redux/reducers/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+// болванка
+const reducer = (state = {}, action) => state;
+
+const rootReducer = combineReducers({ reducer });
+
+export default rootReducer;
