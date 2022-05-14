e09f582651d2096c348013c3b4cb977bcc7d949e
alucardpetrovich <reloaderlev@gmail.com>
2022-05-14 20:29:35 +0300

    fix swagger auth

--- backend/src/modules/rides/rides.controller.ts
+++ backend/src/modules/rides/rides.controller.ts
@@ -11,6 +11,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import {
+  ApiBearerAuth,
   ApiConflictResponse,
   ApiCreatedResponse,
   ApiForbiddenResponse,
@@ -32,6 +33,7 @@ import { RidesListSerializer } from './serializers/rides-list.serializer';
 @Controller('rides')
 @UseGuards(JwtGuard)
 @ApiTags('Rides Controller')
+@ApiBearerAuth()
 export class RidesController {
   constructor(private service: RidesService) {}
 
