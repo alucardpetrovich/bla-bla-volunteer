fe52b9458839b5dffde5d4cd3860805f108ee0cb
Olha-Grim <olha.hrymchak@prea.ws>
2022-05-15 17:16:32 +0200

    add global style

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header';
+import GlobalStyle from '../../utils/styles/global';
 import * as S from './style';
-import { GlobalStyle } from '../../../utils/styles';
 
 interface ILayout {
   children?: any;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
 const middlewares = [thunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers =
+  (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares)),
+);
 
 export default store;
