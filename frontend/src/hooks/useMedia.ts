cafbe366c9d19c0e3f4d16edb84ec79090fe884d
SeregaLucky <strizgsergey@gmail.com>
2022-05-15 22:50:11 +0300

    add when only one media (#19)

--- frontend/src/hooks/useMedia.ts
+++ frontend/src/hooks/useMedia.ts
@@ -3,6 +3,7 @@ import { useMediaQuery } from 'react-responsive';
 interface IUseMedia {
   MOB: boolean;
   TABL: boolean;
+  ONLY_TABL_TEST: boolean;
   DESK: boolean;
   HD: boolean;
 }
@@ -16,10 +17,13 @@ interface IUseMedia {
 const useMedia = (): IUseMedia => {
   const MOB = useMediaQuery({ query: '(max-width: 699px)' });
   const TABL = useMediaQuery({ query: '(min-width: 700px)' });
+  const ONLY_TABL_TEST = useMediaQuery({
+    query: '(min-width: 700px) and (max-width: 1023px)',
+  });
   const DESK = useMediaQuery({ query: '(min-width: 1024px)' });
   const HD = useMediaQuery({ query: '(min-width: 1440px)' });
 
-  return { MOB, TABL, DESK, HD };
+  return { MOB, TABL, ONLY_TABL_TEST, DESK, HD };
 };
 
 export default useMedia;
--- frontend/src/pages/App/index.tsx
+++ frontend/src/pages/App/index.tsx
@@ -1,4 +1,4 @@
-import { useFormatMessage } from '../../hooks';
+import { useFormatMessage, useMedia } from '../../hooks';
 import { homeI18n } from '../../intl/content';
 import useRoutesConstants from '../../hooks/useRoutes';
 import Header from '../../components/Header';
@@ -8,6 +8,8 @@ import Registration from '../Registration';
 export const App = () => {
   const routes = useRoutesConstants();
   const f = useFormatMessage;
+  // const { ONLY_TABL_TEST } = useMedia();
+  // console.log('ONLY_TABL_TEST', ONLY_TABL_TEST);
 
   return (
     <div>
