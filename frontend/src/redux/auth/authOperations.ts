82e82bf42300a36d95f90e8f845bdcb6275e8dc9
Olha-Grim <olha.hrymchak@prea.ws>
2022-05-16 20:39:08 +0200

    add new settings and fix ts and eslint

--- frontend/src/redux/auth/authOperations.ts
+++ frontend/src/redux/auth/authOperations.ts
@@ -1,17 +1,6 @@
-import axios from 'axios';
 import authorizationAPI from '../../api/Auth/Auth';
-/* eslint-disable no-unused-vars */
 import { IAuthCredentials } from '../../models/authModel/authModel';
 import authActions from './authActions';
-
-export const token = {
-  set(token: string) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unSet() {
-    axios.defaults.headers.common.Authorization = '';
-  },
-};
 
 export const userRegistration =
   (credentials: IAuthCredentials) => async (dispatch: any) => {
@@ -20,7 +9,6 @@ export const userRegistration =
       const response = await authorizationAPI.signUp(credentials);
       dispatch(authActions.registrationSuccess(response.data));
     } catch (error) {
-      console.log(error);
       dispatch(authActions.registrationError(error));
     }
   };
