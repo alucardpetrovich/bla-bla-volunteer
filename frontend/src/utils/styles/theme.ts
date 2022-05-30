68cb24e7acb08c5686833f508cfdc55b11e877f3
MarinaSergeyeva <62906557+MarinaSergeyeva@users.noreply.github.com>
2022-05-30 18:04:03 +0300

    Feature/mui (#31)
    * save
    
    * base mui set

---
+++ frontend/src/utils/styles/theme.ts
@@ -0,0 +1,23 @@
+import { deepPurple, grey, lime, yellow } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[400],
+    },
+    secondary: {
+      main: lime[400],
+    },
+    text: {
+      primary: grey[900],
+      secondary: deepPurple[400],
+    },
+    background: {
+      default: '',
+      paper: '',
+    },
+  },
+});
+
+export default theme;
