0bd5145014ef2bdedc54fc6941269094d35ed834
MarinaSergeyeva <62906557+MarinaSergeyeva@users.noreply.github.com>
2022-06-09 15:40:38 +0300

    Fix/font (#33)
    * fonts fixed
    
    * project fonts changed

--- frontend/src/components/StyledComponents/index.tsx
+++ frontend/src/components/StyledComponents/index.tsx
@@ -1,4 +1,4 @@
-export { default as StyledContainer } from './Containers';
-export { default as StyledHeader } from './Headers';
+export { default as Container } from './Containers';
+export { default as Heading } from './Headers';
 export { default as Text } from './Text';
 export { default as Flex } from './Flex';
