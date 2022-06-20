b1ac666aa32df1b4ea382e5d6ef2d3c8355b4c2b
alucardpetrovich <reloaderlev@gmail.com>
2022-06-20 22:05:49 +0300

    fix bug with involvements

--- backend/src/modules/involvements/db/user-involvements.repository.ts
+++ backend/src/modules/involvements/db/user-involvements.repository.ts
@@ -4,10 +4,14 @@ import { UserInvolvementEntity } from './user-involvement.entity';
 @EntityRepository(UserInvolvementEntity)
 export class UserInvolvementsRepository extends Repository<UserInvolvementEntity> {
   async addInvolvements(userId: string, types: string[]) {
-    return this.save(types.map((type) => ({ userId, type })));
+    return this.save(types.map((type) => ({ userId, type, verified: false })));
   }
 
   async removeInvolvements(ids: string[]) {
-    return this.delete({ type: In(ids) });
+    if (!ids.length) {
+      return;
+    }
+
+    return this.delete({ id: In(ids) });
   }
 }
