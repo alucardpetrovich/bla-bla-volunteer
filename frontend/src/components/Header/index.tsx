c5428bba7b8f5d259cb14f4b1f2487668c40ef90
Marina Sergeyeva <morrrinad@gmail.com>
2022-06-29 15:25:06 +0200

    fix: header in user page fixed

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import { Autocomplete, TextField } from '@mui/material';
 import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { generatePath, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-use';
 import { roles } from 'src/constants/roles';
 import { getUser } from 'src/store/user/userSelectors';
 
+import bgImgS from '../../assets/images/header-bg-s.png';
 import { PATHS } from '../../constants/PATH';
 import { userLogOut } from '../../store';
 import { Logo } from '../atoms';
@@ -14,7 +16,7 @@ import Notifications from '../atoms/Notifications';
 import { Container, Heading, Text } from '../StyledComponents';
 import HeaderBg from './components/HeaderBg';
 // import Navigation from './components/Navigation';
-import { HeaderSubtitleWrapper, HeaderTitleWrapper, NavWrapper, SignUpButton } from './style';
+import { HeaderSubtitleWrapper, HeaderTitleWrapper, HeaderWrapper, SignUpButton } from './style';
 
 const Header = () => {
   const { formatMessage } = useIntl();
@@ -25,7 +27,10 @@ const Header = () => {
   const involvements = user?.involvements;
   const userRole = involvements && roles.map(role => (role.id === involvements[0].type ? `${role.title}` : null));
 
+  const { pathname } = useLocation();
   const dispatch = useDispatch();
+
+  const isShowHeading = !!(pathname?.includes('registration') || pathname?.includes('login'));
 
   const handleLogOut = () => {
     if (!isAuth) {
@@ -52,47 +57,122 @@ const Header = () => {
 
   return (
     <header>
-      <HeaderBg isAuth={isAuth}>
-        <Container tag="header" isAuth={isAuth}>
-          <NavWrapper>
-            <Logo
-              height="70px"
-              onClick={() => navigate(generatePath(PATHS.HOME.path, { lang }))}
-              style={{ cursor: 'pointer' }}
-            />
+      {!isAuth && (
+        <HeaderBg isAuth={isAuth} isShowHeading={isShowHeading}>
+          <Container tag="header" isAuth={isAuth} isShowHeading={isShowHeading}>
+            <HeaderWrapper>
+              {!isAuth && (
+                <Logo
+                  height="70px"
+                  onClick={() => navigate(generatePath(PATHS.HOME.path, { lang }))}
+                  style={{ cursor: 'pointer' }}
+                />
+              )}
 
-            {isAuth && (
+              <div style={{ width: '166px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Heading tag="h4" onClick={handleLogOut} style={{ cursor: 'pointer' }}>
+                  {isAuth ? 'вихід' : 'вхід'}
+                </Heading>
+                <div style={{ borderRight: '1px solid grey', height: '24px' }}></div>
+                <Text tag="b1">ua</Text>
+              </div>
+
+              {/* <div style={{ display: 'flex' }}>
+              <Navigation style={{ marginRight: '30px' }} />
+              <p>lang</p>
+            </div> */}
+            </HeaderWrapper>
+            {!isAuth && !isShowHeading && (
               <>
-                <Text tag="b1">{userRole}</Text>
-                <div style={{ width: '360px' }}>
-                  <Autocomplete
-                    freeSolo
-                    id="free-solo-2-demo"
-                    disableClearable
-                    options={roles.map(role => role.title)}
-                    renderInput={params => (
-                      <TextField
-                        {...params}
-                        sx={{ m: 1, width: '36ch', bgcolor: '#fff' }}
-                        size="small"
-                        label="Пошук"
-                        InputProps={{
-                          ...params.InputProps,
-                          // startAdornment: (
-                          //   <InputAdornment position="start">
-                          //     <SearchIcon width={16} />
-                          //   </InputAdornment>
-                          // ),
-                          // placeholder: 'Пошук',
-                          type: 'search',
-                        }}
-                      />
-                    )}
-                  />
-                </div>
-                <Notifications width={24} isNew={true} style={{ cursor: 'pointer' }} />
+                <HeaderTitleWrapper>
+                  <Heading tag="h2" style={{ marginBottom: '200px' }}>
+                    {formatMessage({
+                      defaultMessage: 'Безпечна платформа координації передачі гуманітарної допомоги',
+                      description: 'Header: title',
+                    })}
+                  </Heading>
+                  <HeaderSubtitleWrapper>
+                    <Text tag="b1" style={{ width: '345px' }}>
+                      Зареєструйся та стань частиною українського волонтерського руху
+                    </Text>
+                    <SignUpButton onClick={() => navigate(generatePath(PATHS.REGISTRATION.path, { lang }))}>
+                      <Text tag="b1">реєстрація</Text>
+                      <ArrowRight width={30} />
+                    </SignUpButton>
+                  </HeaderSubtitleWrapper>
+                </HeaderTitleWrapper>
               </>
             )}
+          </Container>
+        </HeaderBg>
+      )}
+      {isAuth && (
+        // <div
+        //   style={{
+        //     width: '250px',
+        //     backgroundImage: `url(${bgImgS})`,
+        //     backgroundRepeat: 'no-repeat',
+        //     backgroundPosition: 'top',
+        //     backgroundSize: 'cover',
+        //   }}
+        // >
+        <Container tag="headerAuth">
+          <div style={{ display: 'flex' }}>
+            <div
+              style={{
+                width: '250px',
+                height: '128px',
+                backgroundImage: `url(${bgImgS})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'top',
+                backgroundSize: 'cover',
+              }}
+            ></div>
+            <Logo
+              height="35px"
+              onClick={() => navigate(generatePath(PATHS.HOME.path, { lang }))}
+              style={{ cursor: 'pointer', position: 'absolute', top: '48px', left: '32px' }}
+            />
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: 'calc(100% - 250px)',
+              padding: '0px 32px',
+            }}
+          >
+            <Text tag="b1">{userRole}</Text>
+            <div style={{ width: '360px' }}>
+              <Autocomplete
+                freeSolo
+                id="free-solo-2-demo"
+                disableClearable
+                options={roles.map(role => role.title)}
+                renderInput={params => (
+                  <TextField
+                    {...params}
+                    sx={{ m: 1, width: '36ch', bgcolor: '#fff' }}
+                    size="small"
+                    label="Пошук"
+                    InputProps={{
+                      ...params.InputProps,
+                      // startAdornment: (
+                      //   <InputAdornment position="start">
+                      //     <SearchIcon width={16} />
+                      //   </InputAdornment>
+                      // ),
+                      // placeholder: 'Пошук',
+                      type: 'search',
+                    }}
+                  />
+                )}
+              />
+            </div>
+            <Notifications width={24} isNew={true} style={{ cursor: 'pointer' }} />
+
             <div style={{ width: '166px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
               <Heading tag="h4" onClick={handleLogOut} style={{ cursor: 'pointer' }}>
                 {isAuth ? 'вихід' : 'вхід'}
@@ -100,33 +180,10 @@ const Header = () => {
               <div style={{ borderRight: '1px solid grey', height: '24px' }}></div>
               <Text tag="b1">ua</Text>
             </div>
-
-            {/* <div style={{ display: 'flex' }}>
-              <Navigation style={{ marginRight: '30px' }} />
-              <p>lang</p>
-            </div> */}
-          </NavWrapper>
-          {!isAuth && (
-            <HeaderTitleWrapper>
-              <Heading tag="h2" style={{ marginBottom: '200px' }}>
-                {formatMessage({
-                  defaultMessage: 'Безпечна платформа координації передачі гуманітарної допомоги',
-                  description: 'Header: title',
-                })}
-              </Heading>
-              <HeaderSubtitleWrapper>
-                <Text tag="b1" style={{ width: '345px' }}>
-                  Зареєструйся та стань частиною українського волонтерського руху
-                </Text>
-                <SignUpButton onClick={() => navigate(generatePath(PATHS.REGISTRATION.path, { lang }))}>
-                  <Text tag="b1">реєстрація</Text>
-                  <ArrowRight width={30} />
-                </SignUpButton>
-              </HeaderSubtitleWrapper>
-            </HeaderTitleWrapper>
-          )}
+          </div>
         </Container>
-      </HeaderBg>
+        // </div>
+      )}
     </header>
   );
 };
