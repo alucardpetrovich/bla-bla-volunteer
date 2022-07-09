0622bb6f9ed66625935506bd28074e1e350fa85e
Olha-Grim <olha.hrymchak@prea.ws>
2022-07-09 16:07:17 +0200

    fix: chenge img to component(Networks)

--- frontend/src/components/Networks/style.ts
+++ frontend/src/components/Networks/style.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const NetworksWrapper = styled.ul`
   display: flex;
+`;
 
-  img {
-    width: 24px;
-    height: 24px;
-    margin-right: 24px;
-    cursor: pointer;
-  }
+export const NetworksImg = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 24px;
+  cursor: pointer;
 `;
