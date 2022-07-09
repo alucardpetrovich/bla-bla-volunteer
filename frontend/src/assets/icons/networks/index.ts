8df9dc4d049c737b8702431bd397b5b4af705bb3
Olha-Grim <78364145+Olha-Grim@users.noreply.github.com>
2022-07-09 18:01:06 +0300

    Feature/implement footer (#52)
    * feat: implement footer
    
    * fix: remove unnecessary colors in global.ts
    
    * fix: add theme color to StyledDivider
    
    * fix: add new color to palette and  change color to colorTheme and transfer some style t0 component
    
    * fix: rewritte the Divider component
    
    * fix: nesting in styles of Footer component
    
    * fix: add translation to Footer
    
    * fix: chenge img to component(Networks)

---
+++ frontend/src/assets/icons/networks/index.ts
@@ -0,0 +1,3 @@
+export { default as InstaIcon } from './Combined-Shape.png';
+export { default as FaceBookIcon } from './Combined-Shape_2.png';
+export { default as TweetIcon } from './Combined-Shape_3.png';
---
+++ frontend/src/components/Divider/index.tsx
@@ -0,0 +1,24 @@
+import { theme } from '@ui-kit';
+import React from 'react';
+
+import * as S from './style';
+
+interface IDivider {
+  type?: number | 1;
+}
+
+/**
+ * @param type has type - "default"
+ * Example: <Divider type={1}/>
+ * @returns
+ */
+
+export const Divider: React.FC<IDivider> = ({ type }) => {
+  switch (type) {
+    case 1:
+      return <S.StyledDivider margin="20px 0px" />;
+
+    default:
+      return <S.StyledDivider margin="40px 0px" color={theme.palette.common.accentBlue} />;
+  }
+};
