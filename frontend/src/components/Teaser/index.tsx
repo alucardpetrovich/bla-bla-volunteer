50461e3e5a0b8eca2de9141895e5c18877967314
Olha-Grim <olha.hrymchak@prea.ws>
2022-07-11 20:40:20 +0200

    fix: ternary operator in condition

--- frontend/src/components/Teaser/index.tsx
+++ frontend/src/components/Teaser/index.tsx
@@ -51,7 +51,7 @@ export const Teaser: React.FC<ITeaser> = ({
           className="teaser_item"
           key={item.title + '' + key}
           onClick={handleClick}
-          isActive={currentIndex === key ? true : !isBlockBubbling ? true : false}
+          isActive={currentIndex === key || !isBlockBubbling}
         >
           <h2>{item.title}</h2>
           <p>{item.description}</p>
